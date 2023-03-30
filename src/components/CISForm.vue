a9cfc5f04ded49798552c11e53cde43efa179eda
boretscy <boretscy@gmail.com>
2023-03-30 14:25:50 +0300

    qweqw

--- src/components/CISForm.vue
+++ src/components/CISForm.vue
@@ -155,7 +155,7 @@ export default {
         
         brandOptions() {
           let s = [...this.$root.brands]
-          return s.sort((a, b) => a.code < b.code ? 1 : -1)
+          return s.sort((a, b) => a.code > b.code ? 1 : -1)
         },
         modelOptions() {return this.$root.models},
 
